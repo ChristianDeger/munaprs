a5cdb04a0a98677707a46f12b037bb85726a728d
Christian Deger <christian@deger.eu>
2011-04-13 17:06:53 +0200

    Committing

--- init.ps1
+++ init.ps1
@@ -1,6 +1,3 @@
-"Copying configuration files"
-cp build\local_properties.rb.customize build\local_properties.rb
-cp build\tasks\configuration_db.rb.customize build\tasks\configuration_db.rb
 
 "Creating the remote link to developwithpassion code base"
 git remote rm jp
